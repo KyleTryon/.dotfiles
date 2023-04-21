482b405c62cced5476ceb363627a7900dc7a0a08
KyleTryon <git@techsquidtv.com>
2023-04-21 10:44:40 -0400

    fix: clone function errors

--- functions/clone.sh
+++ functions/clone.sh
@@ -2,13 +2,15 @@
 clone() {
   local url="$1"
 
-  if [[ -z "$repo" ]]; then
+  if [[ -z "$url" ]]; then
     echo "No repo specified"
     return 1
   fi
 
   git clone "${url}"
   repo=${url##*/}
-  dir="${//\.git//}"
+  echo "Cloned ${repo}"
+  dir="${repo%.*}"
+  echo "Changing directory to ${dir}"
   cd "${dir}" || return
 }
